931069ab1aa3bd3a0f5883dc44d397c47dcd4ae4
Anouska Hopkins <anouskahopkins@gmail.com>
2018-11-13 14:00:23 +0000

    Sinatra app with javascript working

--- public/interface.js
+++ public/interface.js
@@ -2,7 +2,11 @@ $(document).ready(function() {
 
   $('#button').click(function() {
   $.get('https://async-workshops-api.herokuapp.com/people', function(data) {
-    $('#people').text(data);
+    var string = ""
+    for (var i = 0; i < data.length; i++) {
+      string += (data[i].name + ", ")
+    }
+    $('#people').text('People: ' + string.slice(0, -2));
   });
 });
 
